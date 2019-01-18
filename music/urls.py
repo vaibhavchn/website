c08cc08542d481fcc98c4d910981292e29260a68
vaibhavchn <vaibhav.chauhan.674@gmail.com>
2019-01-18 22:18:57 +0530

    Initial commit

---
+++ music/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'music'
+
+
+urlpatterns = [
+    # /music/
+    url(r'^$', views.index, name='index'),
+
+    # /music/742/
+    url(r'^(?P<album_id>[0-9]+)/$', views.details, name='details'),
+
+    # /music/<album_id>/favorite/
+    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name="favorite"),
+]
---
+++ music/views.py
@@ -0,0 +1,6 @@
+from django.views import generic
+from .models import Album
+
+class IndexViews(generic.ListView):
+    template_name = '' \
+                    ''
